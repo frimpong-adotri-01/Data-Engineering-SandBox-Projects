e1c2387a60b43a427411133e430049f65c2ff058
frimpong-adotri-01 <goldmalcom43@gmail.com>
2024-05-09 04:52:16 +0200

    [FEAT] Setup-docker-dev-env: Create 'setup.sh' file

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
-echo "Update and upgrade"
+#!/bin/bash
+
+echo "======================= Update and upgrade ======================="
 apt update
 apt upgrade -y
 
-echo "Install Sudo"
+echo "======================= Install Sudo ======================="
 apt install sudo -y
 
 useradd -rm -d /home/dev -s /bin/bash -g root -G sudo -u 1000 dev && \
@@ -10,16 +12,19 @@ useradd -rm -d /home/dev -s /bin/bash -g root -G sudo -u 1000 dev && \
     echo 'root:xxxx' | chpasswd
 
 
-echo "Install Neofetch"
+echo "======================= Install Neofetch ======================="
 apt install neofetch -y
 
-echo "Install Vim"
+echo "======================= Install Vim ======================="
 apt install vim -y
 
-echo "Install Git"
+echo "======================= Install Curl ======================="
+apt install curl -y
+
+echo "======================= Install Git ======================="
 apt install git -y
 
-echo "Install and setup SSH"
+echo "======================= Install and setup SSH ======================="
 apt install openssh-server -y
 
 # Set up configuration for SSH
@@ -28,3 +33,20 @@ mkdir /var/run/sshd && \
     sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd && \
     echo "export VISIBLE=now" >> /etc/profile
 service ssh start
+
+echo "======================= Setup Exercism configuration ======================="
+cd /home/dev/
+mkdir exercism
+wget "https://github.com/exercism/cli/releases/download/v3.3.0/exercism-3.3.0-linux-x86_64.tar.gz"
+tar -xvzf exercism-3.3.0-linux-x86_64.tar.gz
+mkdir -p ~/bin
+mv exercism ~/bin
+rm -rf *
+echo 'export PATH=~/bin:$PATH' >> ~/.bash_profile
+echo "source ~/.bash_profile">>~/.bashrc
+
+echo "======================= Configurations finales ======================="
+cat /shell_color.txt >> ~/.bashrc
+
+sleep 3
+
